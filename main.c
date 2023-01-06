25a0062f62d61919bcef22202d1709740be5dcaf
Chris-Haj <chrishaj1191@gmail.com>
2023-01-06 23:49:28 +0200

    yes

--- main.c
+++ main.c
@@ -1,10 +1,23 @@
-#include <stdio.h>
-#include "server.c"
-#include "threadpool.h"
-
-int main(int argc, char *argv[]){
-    char date[128];
-    getDate(date);
-    printf("%s\n",date);
-    return 0;
-}
+//#include <stdio.h>
+//#include <unistd.h>
+//#include <string.h>
+//#include <malloc.h>
+//#include <dirent.h>
+//#include <sys/stat.h>
+//#include <fcntl.h>
+////#include "server.c"
+////#include "threadpool.h"
+//
+//
+//
+//int main(int argc, char *argv[]){
+//    char *path = "cmake-build-debug/Testing/index.html";
+//    int fd = open(path, O_RDONLY);
+//    char *buffer = malloc(1024);
+//    while (read(fd, buffer, 50) > 0) {
+////        printf("%s", buffer);
+//    }
+//    printf("\n\n\n\n%s\n",buffer);
+//    close(fd);
+//
+//}
--- server.c
+++ server.c
@@ -3,19 +3,30 @@
 #include <stdlib.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <dirent.h>
+#include <sys/stat.h>
 #include "threadpool.h"
+#include <fcntl.h>
 
 #define alloc(type, size) (type *) malloc(sizeof(type)*size)
 #define RFC1123FMT "%a, %d %b %Y %H:%M:%S GMT"
-
+#define BYTE 1024
 //Command line usage: server <port> <pool-size> <max-number-of-request>
 char *get_mime_type(char *name);
 
-char *createResponse();
+char * createResponse(void *SD);
 
 void requestParse(int argc, char *argv[]);
 
 int socketCreation(int, int maxRequests);
+
+int isFile(char *path) {
+    struct stat path_stat;
+    stat(path, &path_stat);
+    return S_ISREG(path_stat.st_mode);
+}
 
 int main(int argc, char *argv[]) {
     requestParse(argc, argv);
@@ -44,21 +55,112 @@ void getDate(char *date) {
     strftime(date, 128, RFC1123FMT, gmtime(&now));
 }
 
-char *createResponse() {
-    char *response = alloc(char, 1024);
+char * createResponse(void *SD) {
+    int socketDescriptor = *((int *) SD);
+    char buffer[BYTE] = {0};
+    char *response = alloc(char, BYTE*10);
+    memset(response,'\0',BYTE);
+    char *end;
+    while (read(socketDescriptor, buffer, BYTE) > 0)
+        if ((end = strstr(buffer, "\r\n")))
+            break;
+    if (end)
+        *end = '\0';
     char date[128];
     getDate(date);
-    strcpy(response,
-           "(HTTP/1.0, status, phrase)\r\n"
-           "Server: webserver/1.0\r\n"
-           "Data: <data> \r\n"
-           "Location <url> \r\n" // Only if status is 302
-           "Content-Type: <content-type>\r\n"
-           "Content-Length: <content-length>\r\n"
-           "Last Modified: <date>\r\n"
-           "Connection: close\r\n"
-           "\r\n");
-    return response;
+    char *method = strtok(buffer, " ");
+    char *path = strtok(NULL, " ");
+    char curPath[strlen(path)+2];
+    strcpy(curPath,".");
+    strcat(curPath,path);
+    path = curPath;
+    char *protocol = strtok(NULL, "\0");
+    int exists = access(path,F_OK);
+    DIR *dir = opendir(path);
+    int is_file = isFile(path);
+    if (strstr(method, "GET") == NULL) {//send 501 not supported (File 501.txt)
+        sprintf(response, "HTTP/1.0 501 Not supported\r\n"
+                          "Server: webserver/1.0\r\n"
+                          "Date: %s\r\n"
+                          "Content-Type: text/html\r\n"
+                          "Content-Length: 129\r\n"
+                          "Connection: close\r\n\r\n"
+                          "<HTML><HEAD><TITLE>501 Not supported</TITLE></HEAD>\n"
+                          "<BODY><H4>501 Not supported</H4>\n"
+                          "Method is not supported.\n"
+                          "</BODY></HTML>\n", date);
+        printf("501\n");
+    } else if (exists == -1) {// 404 Error
+        sprintf(response, "HTTP/1.0 404 Not Found\r\n"
+                          "Server: webserver/1.0\r\n"
+                          "Date: %s\r\n"
+                          "Content-Type: text/html\r\n"
+                          "Content-Length: 112\r\n"
+                          "Connection: close\r\n"
+                          "\r\n"
+                          "<HTML><HEAD><TITLE>404 Not Found</TITLE></HEAD>\n"
+                          "<BODY><H4>404 Not Found</H4>\n"
+                          "File not found.\n"
+                          "</BODY></HTML>\n", date);
+    } else if (is_file == 0 && path[strlen(path) - 1] != '/') { // 302 Error
+        sprintf(response, "HTTP/1.0 302 Found\r\n"
+                          "Server: webserver/1.0\r\n"
+                          "Date: %s\r\n"
+                          "Location: %s>\r\n"
+                          "Content-Type: text/html\r\n"
+                          "Content-Length: 123\r\n"
+                          "Connection: close\r\n"
+                          "\r\n"
+                          "<HTML><HEAD><TITLE>302 Found</TITLE></HEAD>\n"
+                          "<BODY><H4>302 Found</H4>\n"
+                          "Directories must end with a slash.\n"
+                          "</BODY></HTML>\n", path,date);
+        printf("302\n");
+    } else if (is_file == 0 && path[strlen(path) - 1] == '/') { // Search for index.html in dir
+        char indexPath[strlen(path)+strlen("index.html") + 1];
+        strcpy(indexPath,path);
+        strcat(indexPath,"index.html");
+        if (access(indexPath, F_OK) != -1){
+            int fd = open(indexPath,O_RDONLY);
+            char reader[BYTE] = {0};
+            size_t bytesRead=0;
+            size_t cur;
+            char temp[sizeof(response)]= {0};
+            while ((cur = read(fd,reader,BYTE)) > 0){
+                strcat(temp,reader);
+                bytesRead += cur;
+            }
+            char *mime = get_mime_type(indexPath);
+            sprintf(response, "HTTP/1.0 200 OK\r\n"
+                              "Server: webserver/1.0\r\n"
+                              "Date: %s\r\n"
+                              "Content-Type: %s\r\n"
+                              "Content-Length: %zu\r\n"
+                              "Connection: close\r\n\r\n", date, mime, bytesRead);
+            strcat(response,temp);
+        } else {
+            sprintf(response, "HTTP/1.0 404 Not Found\r\n"
+                              "Server: webserver/1.0\r\n"
+                              "Date: %s\r\n"
+                              "Content-Type: text/html\r\n"
+                              "Content-Length: 112\r\n"
+                              "Connection: close\r\n"
+                              "\r\n"
+                              "<HTML><HEAD><TITLE>404 Not Found</TITLE></HEAD>\n"
+                              "<BODY><H4>404 Not Found</H4>\n"
+                              "File not found.\n"
+                              "</BODY></HTML>\n", date);
+
+        }
+    } else if (is_file == 1) { //Path is a file
+        //If no read perms, send 403 forbidden error
+        //Else return file
+
+    }
+
+    write(socketDescriptor, response, strlen(response));
+    close(socketDescriptor);
+    return NULL;
 }
 
 void requestParse(int argc, char *argv[]) {
@@ -66,7 +168,6 @@ void requestParse(int argc, char *argv[]) {
         printf("Usage: <port> <pool-size> <max-number-of-request>\n");
         exit(1);
     }
-    char *server = argv[0];
     int port = atoi(argv[1]);
     int poolSize = atoi(argv[2]);
     int maxRequests = atoi(argv[3]);
@@ -74,15 +175,17 @@ void requestParse(int argc, char *argv[]) {
     int socketFD = socketCreation(port, maxRequests);
     threadpool *pool = create_threadpool(poolSize);
     int serverSockets, clientLength;
-    while (1) {
+    int i = 0;
+    while (i < maxRequests) {
         clientLength = sizeof(struct sockaddr_in);
         if ((serverSockets = accept(socketFD, (struct sockaddr *) &client, (socklen_t *) &clientLength)) < 0) {
             perror("accept");
             exit(1);
         }
-        dispatch(pool, (void *) serverSockets, NULL);
+        dispatch(pool, (void *) createResponse, (void *) &serverSockets);
+        i++;
     }
-
+    close(socketFD);
 }
 
 int socketCreation(int port, int maxRequests) {
@@ -95,9 +198,8 @@ int socketCreation(int port, int maxRequests) {
         perror("socket");
         exit(1);
     }
-    int connection;
-    if ((connection = bind(fd, (struct sockaddr *) &sockIn, sizeof(sockIn))) < 0) {
-        perror("connection");
+    if (bind(fd, (struct sockaddr *) &sockIn, sizeof(sockIn)) < 0) {
+        perror("bind");
         exit(1);
     }
     listen(fd, maxRequests);
